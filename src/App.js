f3e9f55eb0913aedb28f4b8addd3b4b122d603aa
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-22 09:52:11 -0800

    started datagrid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-
+import DataGrid from 'react-data-grid';
+import data from './data';
+import React from 'react';
 
 export default function App() {
-  return 
-  <>
-  
-  </>;
+
+  const columns = [
+    { key: 'id', name: 'ID' },
+    { key: 'title', name: 'Title' }
+  ];
+
+  const rows = [
+    { id: 0, title: 'Example' },
+    { id: 1, title: 'Demo' }
+  ];
+
+  return (
+    
+    <DataGrid
+      columns={columns}
+      rows={rows}
+    />
+
+  );
 }
