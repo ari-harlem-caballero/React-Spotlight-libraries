8d82c91ff290f414a2661b3d87bfae7afd40c8fd
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-23 12:23:53 -0800

    started makeColumns test

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
     
     <DataGrid
       columns={columns}
-      rows={rows}
+      rows={data}
     />
 
   );
--- src/data-utils.test.js
+++ src/data-utils.test.js
@@ -1,13 +1,16 @@
 //to test functions 
+import { makeColumns } from './data-utils';
 
-test('renders learn react link', () => {
+test('makeColumns should make an array of columns that capitalizes key', () => {
   const expected = [
 
   ];
 
-  const actual = [
-
-  ];
+  const actual = makeColumns([
+    { first_name: 'Paul', last_name: 'Rudd', email: 'youwish@youknew.com', gender: 'Vague', city: 'Chicago', country: 'Mexico', language: 'ASL' },
+    { first_name: 'Cody', last_name: 'Smith', email: 'figureitout@not.com', gender: 'Snail', city: 'Paris', country: 'Korea', language: 'Spanish' },
+    { first_name: 'Rebekah', last_name: 'Rutz', email: 'dontask@mine.com', gender: 'Female', city: 'Grand Rapids', country: 'China', language: 'French' },
+  ]);
 
   expect(actual).toEqual(expected);
 });
