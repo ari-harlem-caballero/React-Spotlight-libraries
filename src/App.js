d660bdc2acd635f60333ac777969b5c8b3aa5a00
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-23 14:01:19 -0800

    started Victory tutorial

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import DataGrid from 'react-data-grid';
 import data from './data';
 import React from 'react';
 import { makeColumns } from './data-utils';
+import { VictoryAxis, VictoryBar, VictoryChart } from 'victory';
 
 export default function App() {
 
@@ -15,12 +16,32 @@ export default function App() {
   //   { id: 1, title: 'Demo' }
   // ];
 
-  return (
-    
-    <DataGrid
-      columns={makeColumns(data)}
-      rows={data}
-    />
+  const newData = [
+    { quarter: 1, earnings: 13000 },
+    { quarter: 2, earnings: 16500 },
+    { quarter: 3, earnings: 14250 },
+    { quarter: 4, earnings: 19000 }
+  ];
 
+  return (
+    <>
+      <DataGrid
+        columns={makeColumns(data)}
+        rows={data}
+      />
+      <VictoryChart domainPadding={20}>
+        <VictoryAxis 
+          tickValues={[1, 2, 3, 4]}
+        />
+        <VictoryAxis 
+          dependentAxis
+        />
+        <VictoryBar 
+          data={newData}
+          x="quarter"
+          y="earnings"
+        />
+      </VictoryChart>
+    </>
   );
 }
